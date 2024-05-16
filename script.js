216d8f313de763031ca2f8cc6acb3539a489ec2f
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-16 21:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let checkbox = document.querySelector("#checkbox");
+let submit = document.querySelector("#submit");
+let user;
+user = JSON.parse(
+  localStorage.getItem("user") ? localStorage.getItem("user") : "{}"
+);
+
+
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function createButton() {
+  let existingUser = document.createElement("button");
+  existingUser.setAttribute("id", "existing");
+  existingUser.innerText = "Login as existing user";
+  submit.parentNode.append(existingUser);
+}
+
+function formSubmit() {
+  if (!isEmptyObject(user)) {
+    createButton();
+    document.querySelector("#existing").addEventListener("click", () => {
+      alert(`Logged is as ${user.username}`);
+    });
+  } else {
+    submit.addEventListener("click", (e) => {
+      e.preventDefault();
+      let name = username.value;
+      let pass = password.value;
+      if (name !== "" && pass !== "") {
+        if (checkbox.checked) {
+          user = {
+            username: name,
+            password: pass,
+          };
+          localStorage.setItem("user", JSON.stringify(user));
+          alert(`Logged in as ${name}`);
+        } else {
+          localStorage.clear();
+          alert(`Logged in as ${name}`);
+        }
+      }
+    });
+  }
+}
+formSubmit();
